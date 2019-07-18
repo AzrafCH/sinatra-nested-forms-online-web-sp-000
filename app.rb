9a896276ed2af168c0cf3a7410a1a1464aeb34f0
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-18 00:11:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,15 @@ module FormsLab
     end
 
     post '/pirates'
+      @pirate = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :show
     end
 
   end
